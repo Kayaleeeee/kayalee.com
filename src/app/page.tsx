b79543dd00d1c5420f94767bd14ac2ab61f7c144
kayaleeeee <gayeon71057@gmail.com>
2025-01-12 11:15:29 +0900

    메인 페이지 추가 및 shader animation 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,177 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { useEffect, useRef } from "react";
+import styles from "./page.module.scss";
+import * as THREE from "three";
+import {
+  renderFragmentShader,
+  renderVertexShader,
+  simulationFragmentShader,
+  simulationVertexShader,
+} from "./utils/shaderUtils";
+
+const Home = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (!container) return;
+    if (typeof window === "undefined") return;
+
+    // Scene, Camera, Renderer
+    const scene = new THREE.Scene();
+    const simScene = new THREE.Scene();
+
+    const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+    camera.position.z = 1;
+
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+      preserveDrawingBuffer: true,
+    });
+
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    container.appendChild(renderer.domElement);
+
+    const mouse = new THREE.Vector2();
+    let frame = 0;
+
+    const width = window.innerWidth * window.devicePixelRatio;
+    const height = window.innerHeight * window.devicePixelRatio;
+
+    const options = {
+      format: THREE.RGBAFormat,
+      type: THREE.FloatType,
+      minFilter: THREE.LinearFilter,
+      magFilter: THREE.LinearFilter,
+      stencilBuffer: false,
+      depthBuffer: false,
+    };
+
+    let rtA = new THREE.WebGLRenderTarget(width, height, options);
+    let rtB = new THREE.WebGLRenderTarget(width, height, options);
+
+    const simMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        textureA: { value: null },
+        mouse: { value: mouse },
+        resolution: { value: new THREE.Vector2(width, height) },
+        time: { value: 0 },
+        frame: { value: 1 },
+      },
+      vertexShader: simulationVertexShader,
+      fragmentShader: simulationFragmentShader,
+    });
+
+    const renderMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        textureA: { value: null },
+        textureB: { value: null },
+      },
+      vertexShader: renderVertexShader,
+      fragmentShader: renderFragmentShader,
+    });
+
+    const plane = new THREE.PlaneGeometry(2, 2);
+    const simQuad = new THREE.Mesh(plane, simMaterial);
+    const renderQuad = new THREE.Mesh(plane, renderMaterial);
+
+    simScene.add(simQuad);
+    scene.add(renderQuad);
+
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+
+    const ctx = canvas.getContext("2d", { alpha: true });
+
+    if (ctx) {
+      // 배경 색상 채우기
+      ctx.fillStyle = "orange";
+      ctx.fillRect(0, 0, width, height);
+
+      // 텍스트 스타일 설정
+      const fontSize = Math.round(250 * window.devicePixelRatio);
+      ctx.fillStyle = "#fef4b8";
+      ctx.font = `bold ${fontSize}px Arial`;
+      ctx.textAlign = "center"; // 가로 중심
+      ctx.textBaseline = "middle"; // 세로 중심
+      ctx.imageSmoothingEnabled = true;
+      ctx.imageSmoothingQuality = "high";
+
+      // 텍스트 그리기
+      ctx.fillText("Kaya Lee", width / 2, height / 2);
+    }
+
+    const textTexture = new THREE.CanvasTexture(canvas);
+    textTexture.minFilter = THREE.LinearFilter;
+    textTexture.magFilter = THREE.LinearFilter;
+    textTexture.format = THREE.RGBAFormat;
+
+    // Animation loop
+    const animate = () => {
+      simMaterial.uniforms.frame.value = frame++;
+      simMaterial.uniforms.time.value = performance.now() / 1000;
+
+      simMaterial.uniforms.textureA.value = rtA.texture;
+      renderer.setRenderTarget(rtB);
+      renderer.render(simScene, camera);
+
+      renderMaterial.uniforms.textureA.value = rtB.texture;
+      renderMaterial.uniforms.textureB.value = textTexture;
+      renderer.setRenderTarget(null);
+      renderer.render(scene, camera);
+
+      const temp = rtA;
+      rtA = rtB;
+      rtB = temp;
+
+      console.log("amimation");
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    const handleMouseMove = (event: any) => {
+      mouse.x = event.clientX * window.devicePixelRatio;
+      mouse.y = (window.innerHeight - event.clientY) * window.devicePixelRatio;
+    };
+
+    const handleMouseOut = () => {
+      mouse.set(0, 0);
+    };
+
+    const handleResize = () => {
+      const width = container.clientWidth * window.devicePixelRatio;
+      const height = container.clientHeight * window.devicePixelRatio;
+
+      renderer.setSize(width, height);
+      simMaterial.uniforms.resolution.value.set(width, height);
+      rtA.setSize(width, height);
+      rtB.setSize(width, height);
+    };
+    container.addEventListener("mousemove", handleMouseMove);
+    container.addEventListener("mouseleave", handleMouseOut);
+    container.addEventListener("resize", handleResize);
+
+    // Clean up
+    return () => {
+      container.removeEventListener("mousemove", handleMouseMove);
+      container.removeEventListener("mouseleave", handleMouseOut);
+      container.removeEventListener("resize", handleResize);
+      container.removeChild(renderer.domElement);
+    };
+  }, []);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div ref={containerRef} className={styles.page}>
+      <main className={styles.main}></main>
+      <footer className={styles.footer}></footer>
     </div>
   );
-}
+};
+
+export default Home;
