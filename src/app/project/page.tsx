1e57826eb889566810803af67f535b204517f50e
kayaleeeee <gayeon71057@gmail.com>
2025-01-18 22:38:40 +0900

    use client에 맞게 generateMetadata 함수 제거 및 page head 추가

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,28 +1,28 @@
 "use client";
 
+import Head from "next/head";
 import { useSmoothScroll } from "../shared/hooks/useSmoothScroll";
 import { Hero } from "./Hero";
 import { MainFeature } from "./MainFeature";
 import { Problem } from "./Problem";
 import { TechStack } from "./TechStack";
 
-export const generateMatadata = () => {
-  return {
-    title: "Rental Assist",
-    description: "카메라 장비 렌탈 관리를 더 쉽게",
-  };
-};
-
 const ProjectPage = () => {
   useSmoothScroll();
 
   return (
-    <div>
-      <Hero />
-      <Problem />
-      <MainFeature />
-      <TechStack lang="KR" />
-    </div>
+    <>
+      <Head>
+        <title>Rental Assist</title>
+        <meta name="description" content="카메라 장비 렌탈 관리를 더 쉽게" />
+      </Head>
+      <div>
+        <Hero />
+        <Problem />
+        <MainFeature />
+        <TechStack lang="KR" />
+      </div>
+    </>
   );
 };
 
